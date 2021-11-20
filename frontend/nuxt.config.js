445132dd5868078dc453720bfae7f4a7b42108bb
Lennart Espe <3391295+lnsp@users.noreply.github.com>
2021-11-21 00:11:00 +0100

    Use own shortcut icon

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -18,7 +18,7 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'shortcuton icon', type: 'image/png', href: '/favicon.png' }
     ]
   },
 
