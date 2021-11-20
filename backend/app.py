27a1173d3614103ecf61c704efab34c913cc730b
Lennart Espe <3391295+lnsp@users.noreply.github.com>
2021-11-20 15:00:37 +0100

    Add some mock data

--- backend/app.py
+++ backend/app.py
@@ -2,22 +2,40 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def status():
-    return jsonify({ 'status': 'ok' })
+    return jsonify({'status': 'ok'})
+
 
 @app.route("/api/all-tags")
 def tags():
-    return jsonify({ 'tags': [] })
+    return jsonify({'tags': ['banana', 'apple', 'pizza', 'burger', 'chicken', 'steak']})
+
 
 @app.route("/api/plan", methods=['POST'])
 def plan():
     excluded = request.json
     # handle excluded recipes, make tag and recipe recommendations
-    return jsonify({ 'recommended': [], 'recipes': [] })
+    return jsonify({'recommended': ['pizza', 'egg', 'chicken'], 'recipes': [
+        {
+            'title': 'Chicago-style pizza',
+            'image': '',
+        },
+        {
+            'title': 'Egg omelett',
+            'image': '',
+        },
+        {
+            'title': 'Ass',
+            'image': ''
+        }
+    ]})
+
 
 @app.route("/api/shop", methods=['POST'])
 def shopping_list():
-    return jsonify({ 'shoppingList': [], 'recipes': [] })
+    return jsonify({'shoppingList': [], 'recipes': []})
+
 
 app.run(host='0.0.0.0')
