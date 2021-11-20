476cae1d4f61791b77220e550f7dfb14b59f5e2f
Martin Eppert <ga48dal@mytum.de>
2021-11-20 17:55:58 +0100

    bin packing working

--- core/bin_packing.py
+++ core/bin_packing.py
@@ -8,13 +8,12 @@ from food_manager import FoodItem
 from recipes_manager import Recipe
 from utils import load_from_pickle
 
-def calculate_ingredients(recipe, food_manager, ingredient_list):
+def calculate_ingredients(recipe, food_manager, ingredient_list, print_=False):
     """
     Adds to ingredient_list the needed ingredients and their quantities
     """
-    for ing in recipe.ingredients:
-        quantity, tags, is_pcs = ing
-        food_item = food_manager.get_food_item_by_tag(tags)
+    for ing in recipe.ingredient_cache:
+        quantity, food_item, is_pcs = ing
         if food_item != None:
             #print(tags, food_item.name)
             q = quantity
@@ -23,16 +22,15 @@ def calculate_ingredients(recipe, food_manager, ingredient_list):
             
             ingredient_list.setdefault(food_item, 0)
             ingredient_list[food_item] += q
-        else: 
-            return False
-    return True
 
 def calculate_waste(ingredient_list):
     waste = 0
+    total = 0
     for ing, q in ingredient_list.items():
         overshoot = math.ceil(q) - q
         waste += overshoot * ing.weight
-    return waste
+        total += q * ing.weight
+    return waste / total
 
 def compute_score(plan, recipes, food_manager):
     ingredient_list = {}
@@ -41,10 +39,14 @@ def compute_score(plan, recipes, food_manager):
     return calculate_waste(ingredient_list)
 
 def pack_bins(days, recipes, food_manager):
-    bins = [random.randrange(len(recipes)) for _ in range(days)]
+    bins = []
+    for _ in range(days):
+        idx = random.randrange(len(recipes))
+        if idx not in bins:
+            bins.append(idx)
     score = compute_score(bins, recipes, food_manager)
 
-    for i in range(100):
+    for i in range(500):
         alter_idx = random.randrange(days)
         value = random.randrange(len(recipes))
         if not value in bins:
@@ -52,25 +54,35 @@ def pack_bins(days, recipes, food_manager):
             bins_new[alter_idx] = value
             score_new = compute_score(bins_new, recipes, food_manager)
 
-            if score_new < score:
+            if score_new < score+random.uniform(0., 0.1):
+                print(score, score_new)
                 score = score_new
                 bins = bins_new
-                print(i)
-                print(score)
     return bins
 
 if __name__ == "__main__":
     recipes = load_from_pickle(file="recipes.pickle")
-    food_manager = food_manager.assemble(food_file="food.pickle")
+    food_manager = food_manager.assemble(food_file="min_food.pickle")
+    
+    recipes = recipes_manager.remove_duplicate_recipes(recipes)
+    recipes = recipes_manager.remove_nonsense_recipes(recipes)
+    
+    for r in recipes:
+        r.cache_ingredients(food_manager)
+
     food_manager.filter_by_has_weight()
     
     bins = pack_bins(3, recipes, food_manager)
     
+    print(bins)
+    
     ingredient_list = {}
     for b in bins:
-        calculate_ingredients(recipes[b], food_manager, ingredient_list)
-        print(ingredient_list)
+        calculate_ingredients(recipes[b], food_manager, ingredient_list, True)
     
+    print("ingredient_list")
+    for ing, amt in ingredient_list.items():
+        print(ing.name, ing.weight, amt)
 
     for b in bins:
         print(recipes[b])
--- core/recipes_manager.py
+++ core/recipes_manager.py
@@ -4,6 +4,7 @@ import get_units
 import get_ingredient
 import hello_fresh
 import pickle
+import food_manager
 from tqdm import tqdm
 
 def build_ingredient(ingredient, yields):
@@ -35,11 +36,39 @@ class Recipe:
         
     def __str__(self):
         ret = "url: {0}\ningredients:".format(self.url)
-        for ing in self.ingredients:
-            ret += "\n" + str(ing)
+        if hasattr(self, "ingredient_cache"):
+            for ing in self.ingredient_cache:
+                quantity, food_item, is_pcs = ing
+                ret += "\n" + food_item.name + " " + str(quantity)
+        else:
+            for ing in self.ingredients:
+                ret += "\n" + str(ing)
         return ret + "\n"
+    
+    def cache_ingredients(self, fm):
+        self.ingredient_cache = []
+        for num, tokens, is_unit in self.ingredients:
+            food = fm.get_food_item_by_tag(tokens)
+            if food != None:
+                self.ingredient_cache.append((num, food, is_unit))
+    
+    def __eq__(self, other):
+        return self.url == other.url
+
+    def __hash__(self):
+        return hash(self.url)
 
 def load_from_pickle(file="recipes.pickle"):
     with open("recipes.pickle", "rb") as file:
         l = pickle.load(file)
         return l
+
+def remove_duplicate_recipes(recipes):
+    return list(set(recipes))
+
+def remove_nonsense_recipes(recipes):
+    ret = []
+    for r in recipes:
+        if len(r.ingredients) > 3:
+            ret.append(r)
+    return ret
