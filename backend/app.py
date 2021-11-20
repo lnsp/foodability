b2aa7ed0521a1291da05285e9228faf37ce9d347
Lennart Espe <3391295+lnsp@users.noreply.github.com>
2021-11-20 04:57:56 +0100

    Add more backend routes

--- backend/app.py
+++ backend/app.py
@@ -2,8 +2,20 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route("/api")
+@app.route("/")
 def status():
     return jsonify({ 'status': 'ok' })
+
+@app.route("/api/tags")
+def status():
+    return jsonify({ 'tags': [] })
+
+@app.route("/api/recipes", methods=['POST'])
+def status():
+    return jsonify({ 'recipes': [] })
+
+@app.route("/api/shopping-list", methods=['POST'])
+def status():
+    return jsonify({ 'items': [] })
 
 app.run(host='0.0.0.0')
